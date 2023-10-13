a8bad86679495c27a8fbe31d5c2db3ac223195f6
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-10-13 21:17:42 +0800

    add sticky navigation

--- js/script.js
+++ js/script.js
@@ -40,6 +40,29 @@ allLinks.forEach(function(link){
   }
   })
 })
+
+/////////////////////////////////////////
+//Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero')
+
+
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  if(ent.isIntersecting === false){
+    document.body.classList.add('sticky')
+  }
+
+  if(ent.isIntersecting){
+    document.body.classList.remove('sticky')
+  }
+},{
+  // in the viewport
+  root:null,
+  threshold: 0,
+  rootMargin: "-80px",
+})
+obs.observe()
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
